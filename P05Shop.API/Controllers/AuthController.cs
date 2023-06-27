5d1361810098fa3a321494af08bd558900c65b45
Tomasz Leś <szkolenia@tomaszles.pl>
2023-06-27 09:26:35 +0200

    Add project files.

---
+++ P05Shop.API/Controllers/AuthController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using P05Shop.API.Services.AuthService;
+using P06Shop.Shared;
+using P06Shop.Shared.Auth;
+
+namespace P05Shop.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+   // [Authorize]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            this._authService = authService;
+        }
+
+        [HttpGet("Secret"), Authorize]
+        public string SecretText()
+        {
+            return "secret";
+        }
+
+        //public async Task<ActionResult<ServiceResponse<string>>> Login()
+        //{
+        //    return Ok("login");
+        //}
+
+        [HttpPost("register")]
+        public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegisterDTO userRegisterDTO)
+        {
+            var user = new User()
+            {
+                Email = userRegisterDTO.Email,
+                Username = userRegisterDTO.Username
+            };
+
+            var response = await _authService.Register(user, userRegisterDTO.Password);
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        
+        }
+    }
+}
---
+++ P05Shop.API/Services/AuthService/AuthService.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P05Shop.API.Models;
+using P06Shop.Shared;
+using P06Shop.Shared.Auth;
+
+namespace P05Shop.API.Services.AuthService
+{
+
+    public class AuthService : IAuthService
+    {
+        private readonly DataContext _context;
+        public AuthService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public Task<ServiceResponse<bool>> ChangePassword(int userId, string newPassword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<string>> Login(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ServiceResponse<int>> Register(User user, string password)
+        {
+            if (await UserExists(user.Email))
+            {
+                return new ServiceResponse<int>
+                {
+                    Success = false,
+                    Message = "User already exists."
+                };
+            }
+
+            // create password hash and salt
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+            
+            // assign hash and salt to user
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            // add user to db
+            await _context.Users.AddAsync(user);
+            // save changes
+            await _context.SaveChangesAsync();
+
+            return new ServiceResponse<int> { Success= true, Data = user.Id, Message = "Registration successful!" };
+
+        }
+
+        public void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            // using statement to dispose of IDisposable objects
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                // generate random salt
+                passwordSalt = hmac.Key;
+                // generate hash
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        public async Task<bool> UserExists(string email)
+        {
+            if(await _context.Users.AnyAsync(x => x.Email.ToLower() == email.ToLower()))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
